4ba429baa91b9fc94c7eeb1f5301dcb8102aa44d
John G <tynanjk@gmail.com>
2021-07-30 23:17:07 -0500

    Create AppController.java

---
+++ src/main/java/com/app/flight/AppController.java
@@ -0,0 +1,12 @@
+package com.app.flight;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AppController {
+    @RequestMapping
+    public String index() {
+        return "index";
+    }
+}
