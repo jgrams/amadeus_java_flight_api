a7c885152d32bad0c13513b2dcf0bf5ea2c8b987
John G <tynanjk@gmail.com>
2021-08-03 06:09:42 -0500

    add flight, cofnirm, order

--- src/main/ui/src/App.js
+++ src/main/ui/src/App.js
@@ -1,22 +1,30 @@
 import React, { useState } from 'react'
 import Locate from './components/Locate';
+import Flight from './components/Flight';
+import Confirm from './components/Confirm';
+import Order from './components/Order';
 
 export default function App() {
-  const [origin, setOrigin] = useState('');
-  const [destination, setDestination] = useState('');
-
-  const originate = (e) => {
-    setOrigin(e.target.value);
-  }
-
-  const destinate = (e) => {
-    setDestination(e.target.value);
-  }
+  const [origin, setOrigin] = useState();
+  const [destination, setDestination] = useState();
+  const [flight, setFlight] = useState();
+  const [confirmation, setConfirmation] = useState();
+  const [order, setOrder] = useState();
 
   return (
     <div>
-      <Locate handleChoice={originate} display={"Destination"}/>
-      <Locate handleChoice={destinate} display={"Origin"}/>
+      <Locate handleChoice={setDestination} display={"Origin"}/>
+      <Locate handleChoice={setOrigin} display={"Destination"}/>
+      { origin && 
+        destination && 
+        <Flight origin={origin} destination={destination} setFlight={setFlight}/> 
+      }
+      { flight &&
+        <Confirm flight={flight} setConfirmation={setConfirmation} /> 
+      }
+      { confirmation &&
+        <Order confirmation={confirmation} order={order} setOrder={setOrder} /> 
+      }
     </div>
   )
 }
