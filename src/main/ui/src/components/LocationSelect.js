34e9a0058502628e364c1f3376fefbeeb1b1fc14
John G <tynanjk@gmail.com>
2021-08-03 06:10:54 -0500

    use react fragment for key

--- src/main/ui/src/components/LocationSelect.js
+++ src/main/ui/src/components/LocationSelect.js
@@ -2,18 +2,18 @@ import React from "react";
 
 function LocationSelect(props) {
     const options = props.data.map((location) =>  
-      <>
+      <React.Fragment key={location.iataCode}>
         <input type="radio" id={location.iataCode} name="select" value={location.iataCode} />
         <label htmlFor={location.iataCode}>
             {location.name + ": " + location.iataCode}
         </label><br></br>
-      </>
+      </React.Fragment>
     );
 
     return (
       <div>
         {props.data.length > 0 &&
-          <form onChange={props.handleChoice}>
+          <form onChange={(e) => props.handleChoice(e.target.value)}>
             {options}
           </form>
         }
